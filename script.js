515ad0344e1ca5624394706772ed9a0cf8440cfb
dsl2022 <2015rpro@gmail.com>
2023-06-01 20:29:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// todo, select the right things
+const shrinkBtn = ""
+const expandBtn = ""
+const photo = ""
+
+shrinkBtn.addEventListener("click", () => {
+    photo.style.transform = "scale(0.5)";
+});
+
+expandBtn.addEventListener("click", () => {
+    photo.style.transform = "scale(1)";
+});
