17d40bf15dcc4ddad34f7db23cd42c1b09b420e2
datalyst1127 <96359837+datalyst1127@users.noreply.github.com>
2023-06-01 17:53:35 -0700

    completed task

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // todo, select the right things
-const shrinkBtn = ""
-const expandBtn = ""
-const photo = ""
+const shrinkBtn = document.getElementById("shrinkBtn");
+const expandBtn = document.getElementById("expandBtn");
+const photo = document.getElementById("photo");
 
 shrinkBtn.addEventListener("click", () => {
-    photo.style.transform = "scale(0.5)";
+  photo.style.transform = "scale(0.5)";
 });
 
 expandBtn.addEventListener("click", () => {
-    photo.style.transform = "scale(1)";
+  photo.style.transform = "scale(1)";
 });
